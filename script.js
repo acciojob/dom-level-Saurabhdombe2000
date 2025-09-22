a2c4a45c371e5a2d3340b83d671408d777fc9da5
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-09-22 11:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const target = document.getElementByid("level");
+let level = 0;
+let current = target;
+
+// traverse upward until nom parent left;
+while (current) {
+	level ++;
+	current = current.parentElement;
+}
+
+// show the result
+alert("The level of the elemnt is: " + level);
